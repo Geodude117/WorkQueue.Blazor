d14225e13cf3a91e8276425dec8cfd0017b9276e
Geovan Inacay <Geovan.Inacay@wescot.co.uk>
2020-12-02 00:26:10 +0000

    Updated component models

---
+++ DomainData.Models/UIModels/GroupData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DomainData.Models.UIModels
+{
+    public class GroupData
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
